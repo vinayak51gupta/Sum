52729068b7571872c64130a10c0ec720fd2ef753
vinayak51gupta <vinayak51gupta@gmail.com>
2024-12-24 18:35:35 +0530

    created the whole program of sum of two variables in java

---
+++ sum.java
@@ -0,0 +1,9 @@
+package Sum;
+
+public class sum {
+    public static void main(String args[]){
+        int a = 50;
+        int b = 30;
+        System.out.print("sum of two variables will be = "+(a+b));
+    }
+}
